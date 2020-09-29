3d5ee968b3530d12202ff8e9d06713138f92f9cf
Ryan Durk <rpdurk@gmail.com>
2020-09-29 16:31:10 -0700

    updated routers for API

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-import express from 'express';
-import routes from './routes';
-
+const express = require('express');
+//need to require the routes folder to direct all other routes
+const routes = require('./routes');
 const app = express();
 
 const port = 3000;
 
+//ensures all the routes go to the correct place
+app.use(express.static('public'));
+
 app.use(routes);
 
-app.listen(3000, () => {
-    console.log('Server started listening on http://localhost:3000');
-});
+app.listen(3000, () => console.log(`server started at http://localhost:3000`));
--- routes/API/index.js
+++ routes/API/index.js
@@ -1,10 +1,12 @@
-const router = require('express').Router();
-const path = require("path");
-
-// Sets up the Express App
-// =============================================================
-const app = express();
-const PORT = 3000;
+import express from 'express';
+import reservationController from '../../../controller/reservationController';
+const router = express.Router();
+// Prepended to every route we declare here /api/reservation
+// /api/reservation
+router.route('/')
+    .get(reservationController.getReservations)
+    .post(reservationController.addReservation);
+export default router;
 
 
 //  HTML Routes
